7759407ce857273bae61966b9671985088f3f2bc
TitusKI <kidumazie@gmail.com>
2025-06-04 23:45:45 +0300

    Add API constants and error handling classes

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String httpBaseUrl =
+      'https://tasker-with-websocket.onrender.com/api';
+  static const String wsBaseUrl = 'wss://tasker-with-websocket.onrender.com';
+}
